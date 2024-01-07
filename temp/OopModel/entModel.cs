92d73908c26373d5f00330e5afdba45402a914e3
Gunawan Widyatmaji <gw2an@yahoo.com>
2024-01-07 10:35:28 +0700

    Init Temp OOP

---
+++ temp/OopModel/entModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopModel
+{
+    internal class entModel
+    {
+    }
+    public abstract class entTBBase
+    {
+        [Key]
+        virtual public int Id { get; set; }
+    }
+
+    public enum enJenAnimal : byte
+    {
+        Mamalia = 1,
+        Betelur = 2
+    }
+    public enum enJenNafas : byte
+    {
+        ParuParu = 1, Insang = 2, Hybrid = 4
+    }
+    public enum enHabitat : byte
+    {
+        Air = 1, Darat = 2
+    }
+
+    public enum enKatAnimal
+    {
+        Melata, Unggas, Verteberata, KakiEmpat, LainLain
+    }
+    public class Animal : entTBBase
+    {
+        public string Nama { get; set; }
+        public enJenAnimal KembangBiak { get; set; }
+        public enHabitat Habitat { get; set; }
+        public enJenNafas Nafas { get; set; }
+        public enKatAnimal Kategori { get; set; }
+        public bool IsBuas { get; set; }
+    }
+
+    public class Unggas : Animal
+    {
+        public bool IsTerbang { get; set; }
+        public bool IsKicau { get; set; }
+    }
+
+    public class KakiEmpat : Animal
+    {
+
+    }
+}
